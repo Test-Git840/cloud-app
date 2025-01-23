6251897b9a2c82d074b787944f9259ca8a662953
Test-Git840 <studyportalautomation3@gmail.com>
2025-01-23 15:37:44 +0530

    Application created

--- src/main/java/com/spring/demo/DemoApplication.java
+++ src/main/java/com/spring/demo/DemoApplication.java
@@ -2,12 +2,25 @@ package com.spring.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(path = "/cloud")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@GetMapping(path = "/greet", produces = MediaType.TEXT_PLAIN_VALUE)
+	public ResponseEntity<String> greetMsg() {
+		return ResponseEntity.ok("Successful");
 	}
 
 }
